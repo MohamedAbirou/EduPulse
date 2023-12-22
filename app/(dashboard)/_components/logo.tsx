4e9d0b4333884a0936d81a49fe96fc6a0396bff4
Mohamed Abirou <abiroumohamed58@gmail.com>
2023-12-22 13:48:20 +0100

    style: course creation UI

--- app/(dashboard)/_components/logo.tsx
+++ app/(dashboard)/_components/logo.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export const Logo = () => {
   return (
-    <div className="flex items-center space-x-2">
+    <Link href="/" className="flex items-center space-x-2">
       <Image src="/logo.png" width={30} height={30} alt="logo" />
       <p className="text-lg font-semibold mt-2 text-rose-400">EduPulse</p>
-    </div>
+    </Link>
   );
 };
