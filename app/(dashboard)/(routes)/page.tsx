8c9358dc1794b3e508164bf9ef747f24977312b8
Mohamed Abirou <abiroumohamed58@gmail.com>
2023-12-26 17:16:36 +0100

    feat: Student dashboard

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,11 +1,38 @@
-import { UserButton } from "@clerk/nextjs";
+import { GetDashboardCourses } from "@/actions/get-dashboard-courses";
+import { CoursesList } from "@/components/courses-list";
+import { auth } from "@clerk/nextjs";
+import { CheckCircle, Clock } from "lucide-react";
+import { redirect } from "next/navigation";
+import { InfoCard } from "./(root)/_components/info-card";
 
-const Home = () => {
+const Dashboard = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const { completedCourses, coursesInProgress } = await GetDashboardCourses(
+    userId
+  );
   return (
-    <div>
-      <UserButton afterSignOutUrl="/" />
+    <div className="p-6 space-y-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+        <InfoCard
+          icon={Clock}
+          label="In Progress"
+          numberOfItems={coursesInProgress.length}
+        />
+        <InfoCard
+          icon={CheckCircle}
+          label="Completed"
+          variant="success"
+          numberOfItems={completedCourses.length}
+        />
+      </div>
+      <CoursesList items={[...coursesInProgress, ...completedCourses]} />
     </div>
   );
 };
 
-export default Home;
+export default Dashboard;
